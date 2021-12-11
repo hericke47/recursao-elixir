18e268eeb51decd98909f26ddd92ad6e325c14b2
Herick Exterkoetter <herick.exterkoetter@gam.com.br>
2021-12-10 23:24:03 -0300

    feat: sum list using recursive funcions

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: get_length(list, 0)
+
+  def get_length([], acc) do
+    acc
+  end
+
+  def get_length([head | tail] = list, acc) do
+    acc = acc + 1
+    get_length(tail, acc)
   end
 end
